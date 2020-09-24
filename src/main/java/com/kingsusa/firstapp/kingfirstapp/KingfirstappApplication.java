a006e31bac303c2d33933ef18e25e64107f4f484
jilsushil.panwar <sushil.tech91@gmail.com>
2020-09-24 08:35:56 +0530

    [Initial commit]

---
+++ src/main/java/com/kingsusa/firstapp/kingfirstapp/KingfirstappApplication.java
@@ -0,0 +1,13 @@
+package com.kingsusa.firstapp.kingfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingfirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingfirstappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingsusa/firstapp/kingfirstapp/KingfirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingsusa.firstapp.kingfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingfirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
